8278fb75365130b129736cca48128ec1f1c1f9f5
Manan R Patel <mananrpatel7144@gmail.com>
2022-05-19 12:35:30 +0530

    Here We Connect our PostgresSQL database

--- app.py
+++ app.py
@@ -1,15 +1,40 @@
 from flask import Flask
-from flask_restful import ResourceApi
+from flask_restful import Api
+from Resource.User import User
+from db import db
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 
 app = Flask(__name__)
 api = Api(app)
 
-class Test(Resource):
-    @classmethod
-    def get(cls):
-        return "World!"
 
-api.add_resource(Test,'/')
+host = os.getenv('HOST')
+user = os.getenv('USER')
+password = os.getenv('PASSWORD')
+database = os.getenv('DATABASE')
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user}:{password}@{host}/{database}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+@app.route('/')
+def root():
+    return {"Welcome":"Welcome to Cloud Storage"}
+
+api.add_resource(User,'/user')
+
+@app.errorhandler(404)
+def not_found(e):
+
+  return {"Error":"No Route"}
 
 if __name__ == '__main__':
+    db.init_app(app)
     app.run(port=5000, debug=True)
